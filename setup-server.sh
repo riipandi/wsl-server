72a4d85192642cd584ac1a7e5b850c17b69cf64c
Aris Ripandi <ar.is@outlook.com>
2019-01-09 22:08:12 +0700

    Update setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -90,7 +90,7 @@ curl -sS https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 
 echo 'deb https://dl.yarnpkg.com/debian/ stable main' > /etc/apt/sources.list.d/yarn.list
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-apt update && apt install -y nodejs yarn
+apt update ; apt full-upgrade -y ; apt install -y nodejs yarn
 
 # phpMyAdmin
 if [ -d /var/www/myadmin ]; then rm -fr /var/www/myadmin ; fi
